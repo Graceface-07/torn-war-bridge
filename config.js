ca04f489bede9ddb71039ba916c0fb48e539eb9e
Graceface-07 <87267258+Graceface-07@users.noreply.github.com>
2026-01-28 01:27:36 +0000

    Add configuration file with API keys, user ID, faction options, weapon multipliers, color mappings, and tier thresholds.

---
+++ config.js
@@ -0,0 +1,23 @@
+const config = {
+    TORN_API_KEY: 'your_torn_api_key',
+    SC_KEY: 'your_sc_key',
+    USER_ID: 2702970,
+    FACTION_OPTIONS: [38761, 42505],
+    WEAPON_MULTIPLIERS: {
+        'pistol': 1,
+        'knife': 2,
+        'rifle': 3
+    },
+    COLOR_MAPPINGS: {
+        'high': '#FF0000',
+        'medium': '#FFFF00',
+        'low': '#00FF00'
+    },
+    TIER_THRESHOLDS: {
+        'bronze': 0,
+        'silver': 1000,
+        'gold': 5000
+    }
+};
+
+module.exports = config;
